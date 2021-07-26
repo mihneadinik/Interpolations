e59c93e91faf9bfe9df69bf00beef29c63dbd76e
mihneadinik <73951956+mihneadinik@users.noreply.github.com>
2021-07-26 22:31:54 +0300

    Add files via upload

---
+++ nearest-neighbour/nn_2x2_RGB.m
@@ -0,0 +1,20 @@
+function out = nn_2x2_RGB(img, STEP = 0.1)
+    % =========================================================================
+    % Aplica interpolare nearest neighbour pe imaginea 2x2 img cu puncte
+    % intermediare echidistante.
+    % img este o imagine colorata RGB.
+    % =========================================================================
+
+    % TODO: extrage canalul rosu al imaginii
+
+    % TODO: extrage canalul verde al imaginii
+
+    % TODO: extrace canalul albastru al imaginii
+
+
+    % TODO: aplica functia nn pe cele 3 canale ale imaginii
+
+    % TODO: formeaza imaginea finala cu cele 3 canale de culori
+    % Hint: functia cat
+
+endfunction
